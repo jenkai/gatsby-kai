35a50c0622c75889da36e6a8d05ef6641e9fe8f4
Jen Kai You <youjenkai@gmail.com>
2021-07-21 11:18:23 +0800

    white theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,11 +112,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Jenkai's Blog`,
+        short_name: `Jenkai's Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
